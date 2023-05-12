56f7a34c16948f68342fe0787c4618dd896dcc0d
RozetaMeshi <122020803+RozetaMeshi@users.noreply.github.com>
2023-05-12 15:47:50 +0200

    update

--- src/main/java/com/example/det3_ds/SignUpController.java
+++ src/main/java/com/example/det3_ds/SignUpController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+
+
 public class SignUpController {
     @FXML
     private TextField txtName;
@@ -22,4 +24,5 @@ public class SignUpController {
         String email=this.txtEmail.getText();
         String password=this.pwdPassword.getText();
     }
+
 }
